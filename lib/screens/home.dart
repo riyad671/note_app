d52c6610ac873a43693825da823cff08243245f8
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-04 20:59:15 +0600

    Connect home.dart's "tile" to note.dart's content

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:note_app/models/note.dart';
+// import 'package:intl/intl_browser.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -79,50 +82,59 @@ class _HomeScreenState extends State<HomeScreen> {
 
             // Note tiles
             Expanded(
-                child: ListView(
-              children: [
-                Card(
+                child: ListView.builder(
+              itemCount: sampleNotes.length,
+              itemBuilder: (context, index) {
+                return Card(
                   elevation: 3,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10)),
-                  child: ListTile(
-                    title: RichText(
-                      text: const TextSpan(
-                          text: 'Title\n',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            height: 1.5,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: 'Write here',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.normal,
-                                fontSize: 14,
-                                height: 1.5,
-                              ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: ListTile(
+                      title: RichText(
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        // Title
+                        text: TextSpan(
+                            text: '${sampleNotes[index].title}\n',
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              height: 1.5,
                             ),
-                          ]),
-                    ),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Edited: 04-10-2023',
-                        style: TextStyle(
-                          fontSize: 10,
-                          fontStyle: FontStyle.italic,
-                          color: Colors.grey.shade800,
+                            // Sub-title
+                            children: [
+                              TextSpan(
+                                text: '${sampleNotes[index].content}',
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 14,
+                                  height: 1.5,
+                                ),
+                              ),
+                            ]),
+                      ),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        // Modification time
+                        child: Text(
+                          'Edited: ${DateFormat('EEEE MMM d, yyyy h:mm a').format(sampleNotes[index].modifiedTime)}',
+                          style: TextStyle(
+                            fontSize: 10,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.grey.shade800,
+                          ),
                         ),
                       ),
+                      trailing: IconButton(
+                          onPressed: () {}, icon: const Icon(Icons.delete)),
                     ),
-                    trailing: IconButton(
-                        onPressed: () {}, icon: const Icon(Icons.delete)),
                   ),
-                ),
-              ],
+                );
+              },
             )),
           ],
         ),
